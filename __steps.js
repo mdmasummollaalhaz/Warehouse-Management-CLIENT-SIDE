db7bade6633ab4b263a59669dc7eef783ff14306
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-08 10:20:35 +0600

    firebase and dependencies

--- __steps.js
+++ __steps.js
@@ -19,4 +19,14 @@
  * 1. npx create-react-app
  * 2. steps file create
  * 3. Components create
+ * 4. firebase installed
+ * 5. create firebase.init.js and .env.local
+ * 6. npm install react-router-dom@6
+ * 7. npm install --save react-firebase-hooks
+ * 8. npm install --save react-toastify
+ * 9. npm i react-bootstrap
+ * 10. npm i bootstrap
+ * 11. npm i react-hook-form
+ * 12. npm i axios
+ * 13. npm i dotenv
  */
